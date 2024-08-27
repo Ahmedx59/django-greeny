c74a8085900f4d020d3c4e5c144bc6c15b2d20b0
Ahmedx59 <Ahmedx59@gmail.com>
2024-08-27 19:55:00 +0300

    design prododuct model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product ,productReview ,ProductImages ,category ,Brand
+
+
+admin.site.register(Product)
+admin.site.register(productReview)
+admin.site.register(ProductImages)
+admin.site.register(category)
+admin.site.register(Brand)
