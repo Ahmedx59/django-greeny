32458a8c5c9d31da2cc5e564bf7589ce93229268
Ahmedx59 <Ahmedx59@gmail.com>
2024-09-03 21:20:27 +0300

    add product review view , page

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render
 from django.views import generic
-from .models import Product , ProductImages
+from .models import Product , ProductImages , productReview
  
 
 
@@ -18,4 +18,6 @@ class ProductDetail(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['product_image'] = ProductImages.objects.filter(product = self.get_object())
+        context["reviews"] = productReview.objects.filter(product = self.get_object())
         return context
+    
