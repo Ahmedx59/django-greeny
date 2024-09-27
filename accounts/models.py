b313b256bd1c7726b63b1904e588ab1df3ec8f3c
Omarmoatz <omar.raouf.002@gmail.com>
2024-09-27 18:38:11 +0300

    add  profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+    
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    image = models.ImageField( upload_to='profile-img', blank=True, null=True)
+    phone_num = models.CharField( max_length=50, blank=True)
+    address = models.CharField( max_length=50, blank=True)
+    facebook = models.URLField( max_length=200, blank=True)
+    twitter = models.URLField( max_length=200, blank=True)
+    code = ''
+
+    def __str__(self) -> str:
+        return self.user
+
+@receiver(post_save, sender=User)
+def profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user= instance
+        )
